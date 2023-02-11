df214d6709e1ef463958b2deb53438816efb9a69
awaas <alexandra.waas@hof-university.de>
2023-02-11 19:22:24 +0100

    Fixed Bug in imports that caused build crash

--- Assets/Scripts/MainMenuTriggerArea.cs
+++ Assets/Scripts/MainMenuTriggerArea.cs
@@ -1,9 +1,6 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.Experimental.GraphView;
+
 using UnityEngine;
-using UnityEngine.UI;
+
 using Valve.VR.Extras;
 
 public class MainMenuTriggerArea : MonoBehaviour
